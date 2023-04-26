fafe7c7c14dc85cb28e53eb8303a3f2b768d13d5
Atophite <wesley-reinders@outlook.com>
2023-04-26 22:58:18 +0200

    fix

--- index.mjs
+++ index.mjs
@@ -25,7 +25,9 @@ function sendNotification() {
     console.log("discord notification sent")
 }
 
+console.log("welcome to PaceMan bot")
 tmiClient.connect();
+
 tmiClient.on("connecting", () => {
     console.log("connecting with chat...")
 }) 
